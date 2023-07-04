c013de389534d5381bd35576f3b52a0f6163c6c9
risewitholu <138177933+risewitholu@users.noreply.github.com>
2023-07-04 21:20:07 +0100

    Update Badge.tsx
    risewith.io -> risewith

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -61,7 +61,7 @@ export const Badge = (props: Props) => {
         id="lite-badge"
         style={{ "font-weight": 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> RiseWith.io </span>
+        <span> RiseWith</span>
       </a>
     </span>
   )
